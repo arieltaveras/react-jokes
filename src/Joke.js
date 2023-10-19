e8ebe3659a16f3e3eafdeccd2c596034d333ac06
arieltaveras <ariel.taveras7278@gmail.com>
2023-10-19 12:14:34 -0400

    removed custom hook and added sorting by votes

--- src/Joke.js
+++ src/Joke.js
@@ -1,21 +1,20 @@
 import React from "react";
-import useCounter from "./Hooks/useCounter";
 import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
 
 // function Joke({ id, vote, votes, text }) {
-  function Joke({text, id}) {
-    const [ votes, like, unLike ] = useCounter();
+  function Joke({text, id, vote, votes}) {
+
     
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-          <button onClick={like}>
+          <button onClick={evt => vote(id, 1)}>
             <i className="fas fa-thumbs-up" />
           </button>
 
-          <button onClick={unLike}>
+          <button onClick={evt => vote(id, -1)}>
             <i className="fas fa-thumbs-down" />
           </button>
 
